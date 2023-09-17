0541bdeea14832bb03371976462b2782c3552137
Demian S. <demslob@gmail.com>
2023-09-17 21:13:57 +0000

    win jar

--- API/src/main/java/org/sikuli/script/runners/ServerRunner.java
+++ API/src/main/java/org/sikuli/script/runners/ServerRunner.java
@@ -521,6 +521,7 @@ public class ServerRunner extends AbstractScriptRunner {
 
       //TODO int retval = Runner.run(fScript.toString(), args);
       int retval;
+      
       try {
         retval = Runner.run(fScript.toString(), args, null);
       } catch (Exception ex) {
